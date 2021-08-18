0d7f0da8d762e1f55bbbb9ce846ec2c2892301df
ackrok <51799203+ackrok@users.noreply.github.com>
2021-08-18 14:59:33 -0400

    Update processReward.m

--- Process/processReward.m
+++ Process/processReward.m
@@ -26,22 +26,30 @@ Fs = rawFs/dsRate;
 data.gen.Fs = Fs;
 sigEdge = params.FP.sigEdge;
 for n = 1:nAcq
-    lick = data.acq(n).lick; % Extract raw lick trace from data structure for processing
-    rewDelivery = data.acq(n).rew; % Extract raw cue/reward trace from data structure for processing
-    if sigEdge ~= 0 % Remove the beginning and the edge if the setting isn't 0
-        lick = lick((sigEdge*rawFs)+1:end-(sigEdge*rawFs));
-        rewDelivery = rewDelivery((sigEdge*rawFs)+1:end-(sigEdge*rawFs));
+    if isfield(data.acq(n),'lick')
+        choice = menu('Choose lick signal to process:',data.acq(n).lickNames); % Select signal to extract
+        lick = data.acq(n).lick{choice}; % Extract raw lick trace from data structure for processing
+        if sigEdge ~= 0 % Remove the beginning and the edge if the setting isn't 0
+            lick = lick((sigEdge*rawFs)+1:end-(sigEdge*rawFs));
+        end
+        data.final(n).lick.trace = lick(1:dsRate:end); % Downsample lick trace to match Fs of photometry signal
+        data.final(n).lick.onset = getPulseOnsetOffset(lick, 0.5)/dsRate; % Extract lick onset times from raw lick signal/dsRate
     end
-    data.final(n).lick.trace = lick(1:dsRate:end); % Downsample lick trace to match Fs of photometry signal
-    data.final(n).lick.onset = getPulseOnsetOffset(lick, 0.5)/dsRate; % Extract lick onset times from raw lick signal/dsRate
-    data.final(n).rew.trace = rewDelivery(1:dsRate:end); % Downsample reward trace to match Fs of photometry signal
-    [onset, offset] = getPulseOnsetOffset(rewDelivery, 0.5); % Extract cue and reward onset times from raw signal
-    onset = onset/dsRate; offset = offset/dsRate; % Adjust trace rise and falls for Fs
-    data.final(n).rew.cue = onset( offset-onset > mean(offset-onset) ); % Long pulses are cue signal
-    data.final(n).rew.delivery = onset( offset-onset < mean(offset-onset) ); % Short pulses are reward delivery signal
-    L = length(lick);
-    L = length(1:dsRate:L); % Length of signal, after removing sigEdge
-    timeVec = [1:L]/Fs; % Generate time vector using length of signal, Fs of downsampled signal
-    data.final(n).time = timeVec'; % Load time vector into data structure
+    if isfield(data.acq(n),'rew')
+        choice = menu('Choose reward signal to process (includes cue, solenoid):',data.acq(n).rewNames); % Select signal to extract
+        rewDelivery = data.acq(n).rew{choice}; % Extract raw cue/reward trace from data structure for processing
+        if sigEdge ~= 0 % Remove the beginning and the edge if the setting isn't 0
+            rewDelivery = rewDelivery((sigEdge*rawFs)+1:end-(sigEdge*rawFs));
+        end
+        data.final(n).rew.trace = rewDelivery(1:dsRate:end); % Downsample reward trace to match Fs of photometry signal
+        [onset, offset] = getPulseOnsetOffset(rewDelivery, 0.5); % Extract cue and reward onset times from raw signal
+        onset = onset/dsRate; offset = offset/dsRate; % Adjust trace rise and falls for Fs
+        data.final(n).rew.cue = onset( offset-onset > mean(offset-onset) ); % Long pulses are cue signal
+        data.final(n).rew.delivery = onset( offset-onset < mean(offset-onset) ); % Short pulses are reward delivery signal
+        if ~isfield(data.final(n),'time')
+            timeVec = [1/Fs : 1/Fs : (length(rawDelivery)/dsRate/Fs)]'; % Generate time vector using length of signal, Fs of downsampled signal
+            data.final(n).time = timeVec; % Load time vector into data structure
+        end
+    end
 end
 end
